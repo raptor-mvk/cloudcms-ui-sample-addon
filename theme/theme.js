f69636faae43f9857f97aecdcb688b71524975ad
raptor_MVK <m.v.kamorin@gmail.com>
2019-12-24 22:45:59 +0300

    Fix

--- theme/theme.js
+++ theme/theme.js
@@ -1,8 +1,5 @@
 define(function(require, exports, module) {
     require("css!bootstrap/../../css/bootstrap.css");
     require("css!app/../../main.css");
-
-    const $ = require("jquery");
-
-    $('.btn-header-help').hide();
+    require("css!./theme.css");
 });
