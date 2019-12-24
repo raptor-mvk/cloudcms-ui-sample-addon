f8c03437c7b132bb0317f6a2c4ed64a35e00ef24
raptor_MVK <m.v.kamorin@gmail.com>
2019-12-24 21:41:42 +0300

    Fix error

--- theme/index.js
+++ theme/index.js
@@ -2,13 +2,12 @@ define(function(require, exports, module) {
 
     var UI = require("ui");
     var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
-    console.log(moduleId);
 
     // register the theme: "hidden-header-theme"
     UI.registerTheme({
         "key": "hidden-header-theme",
         "title": "Hidden header Theme",
-        "module": moduleId + "/theme.js"
+        "module": moduleId + "/hidden-header-theme/theme.js"
     });
 
 });
