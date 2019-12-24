8d6928d83a2a3fe6b370fb327e408fce4667f6fa
raptor_MVK <m.v.kamorin@gmail.com>
2019-12-24 19:57:19 +0300

    Add theme

--- theme/theme.js
+++ theme/theme.js
@@ -3,7 +3,6 @@ define(function(require, exports, module) {
     require("css!bootstrap/../../css/bootstrap.css");
     require("css!app/../../main.css");
 
-    require("css!themes/theme-reset.css");
     require("css!./theme.css");
 
 });
