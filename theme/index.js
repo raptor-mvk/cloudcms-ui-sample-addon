1d209ca77857a1d19902932fc22918645ebdef39
raptor_MVK <m.v.kamorin@gmail.com>
2019-12-24 21:34:31 +0300

    Debug

--- theme/index.js
+++ theme/index.js
@@ -1,9 +1,10 @@
 define(function(require, exports, module) {
 
     var UI = require("ui");
+    console.log(module);
     var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
 
-    // register the theme: "arrested-development-theme"
+    // register the theme: "hidden-header-theme"
     UI.registerTheme({
         "key": "hidden-header-theme",
         "title": "Hidden header Theme",
